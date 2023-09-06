dc423be374036c713a13b355ed90626d999cbd81
Wolf McNally <wolf@wolfmcnally.com>
2023-09-06 01:58:42 -0700

    Aligned to dependencies.

--- Sources/EnvelopeTool/Commands/Main.swift
+++ Sources/EnvelopeTool/Commands/Main.swift
@@ -6,7 +6,7 @@ struct Main: ParsableCommand {
     static var configuration = CommandConfiguration(
         commandName: "envelope",
         abstract: "A tool for manipulating the Envelope data type.",
-        version: "0.12.0",
+        version: "0.12.1",
         shouldDisplay: false,
         subcommands: [
             AssertionCommand.self,
